f859736664acef31ef7e2c940ca854e7036e7e89
YeonJae <duswo9595@naver.com>
2022-06-07 11:53:29 +0900

    typescript 기본개념 : 별칭

---
+++ TypeAlias.ts
@@ -0,0 +1,16 @@
+type Age = number;
+type Player = {
+  name: string;
+  age?: Age;
+};
+
+//Type Alias
+
+const yj: Player = {
+  name: "yeonjae",
+};
+
+const mj: Player = {
+  name: "minjae",
+  age: 28,
+};
