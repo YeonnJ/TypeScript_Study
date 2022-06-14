c5b00960d90d5b46c2b86a76d90dd7c15b4c9ea8
YeonJae <duswo9595@naver.com>
2022-06-15 00:29:41 +0900

    generic

--- Polymorphism/Generic.ts
+++ Polymorphism/Generic.ts
@@ -1,14 +1,38 @@
-type SuperPrint = {
-  <TypePlaceholder>(arr: TypePlaceholder[]): TypePlaceholder;
-  //generic = placeholder
+// type SuperPrint = <T, V>(arr: T[], str: V) => T;
+//generic = placeholder
 
-  // (arr: number[]):void
-  // (arr: boolean[]):void
-  // (arr: string[]):void
+// (arr: number[]):void
+// (arr: boolean[]):void
+// (arr: string[]):void
+
+// const superPrint: SuperPrint = (arr) => arr[0];
+
+function SuperPrint<T>(a: T[]) {
+  return a[0];
+}
+
+SuperPrint([1, 2, 3, 4]);
+SuperPrint([true, false, false]);
+SuperPrint(["hi", "bye"]);
+
+//example
+type Player<E> = {
+  name: string;
+  extraInfo: E; //generic type 생략도가능
 };
 
-const superPrint: SuperPrint = (arr) => arr[0];
+type FavFood = {
+  favFood: string;
+};
 
-superPrint([1, 2, 3, 4]);
-superPrint([true, false, false]);
-superPrint(["hi", "bye"]);
+const YJ: Player<FavFood> = {
+  name: "yj",
+  extraInfo: {
+    favFood: "apple",
+  },
+};
+
+//example
+type A = Array<number>; //generic
+
+let a: A = [1, 2, 3];
